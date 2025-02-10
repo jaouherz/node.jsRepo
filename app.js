f7da304228f36696280b032af24fbaad5f09b281
jaouher2002 <jaouher2002@gmail.com>
2025-02-10 13:41:51 +0100

    séance 3

--- app.js
+++ app.js
@@ -1,41 +1,29 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+var router = express.Router();
+var app= express();
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+module.exports = router;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.get('/',(req,res)=>{
 
-module.exports = app;
+  res.send({message:"hello to  my website"})
+  res.end()
+},)
+app.listen(8082,()=>{
+
+      console.log("Server running on port 8080")
+    }
+
+
+
+)
+
+app.get('/file',(req,res)=>{
+
+  res.sendFile(__dirname+'/public/index.html');
+
+
+})
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+var app= express();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+app.get('/',(req,res)=>{
+
+  res.send({message:"hello to  my website"})
+  res.end()
+    },)
+app.listen(8080,()=>{
+
+  console.log("Server running on port 8080")
+}
+
+
+
+)
+
+app.get('/file',(req,res)=>{
+
+  res.sendFile(__dirname+'/../public/index.html');
+
+
+})
